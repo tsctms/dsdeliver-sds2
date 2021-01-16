55dc20c63a7c50f90c03dcd09218cdd29725714f
Thiago Souza <thiagonk10fta@hotmail.com>
2021-01-15 21:10:58 -0300

    Add detalhe pedido

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, Alert} from 'react-native';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard'
+import { Order } from '../types';
 
 
 function Orders() {
 
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoad, setIsLoad] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  
+  const fetchData =() => {
+    setIsLoad(true)
+    fetchOrders()
+    .then(response => setOrders(response.data))
+    .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
+    .finally(() => setIsLoad(false));
+  }
+
+  useEffect(() =>{
+    if (isFocused){
+      fetchData();
+    }
+  }, [isFocused]);
 
   
+  const handleOnPress = (order: Order) =>{
+    navigation.navigate('OrdersDetails', {
+      order
+    });
+  }
+
   return (
    <>
       <Header />
       <ScrollView style={styles.container}>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {isLoad ? (
+          <Text>Buscando pedidos...</Text>
+        ) : (
+          orders.map(order => (
+            <TouchableWithoutFeedback key ={order.id} onPress ={() => handleOnPress(order)}>
+              <OrderCard order={order} />
+            </TouchableWithoutFeedback>
+          ))
+        )}
       </ScrollView>
    </>
   )
